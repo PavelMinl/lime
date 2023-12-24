9c0f86eb7558a559ce7374c27d5ba86465fa084f
Pavel <mindalev77@mail.ru>
2023-12-24 16:31:21 +0300

    3

--- js/main/script.js
+++ js/main/script.js
@@ -21,3 +21,22 @@ document.addEventListener('DOMContentLoaded', function () {
         },
     });
 });
+document.addEventListener('DOMContentLoaded', function () {
+    const swiper = new Swiper(".layout_swiper", {
+        spaceBetween: 10,
+        slidesPerView: 2,
+        freeMode: true,
+        watchSlidesProgress: true,
+
+    });
+    var swiper2 = new Swiper(".layout_swiper--2", {
+        spaceBetween: 10,
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        thumbs: {
+            swiper: swiper,
+        },
+    });
+});
