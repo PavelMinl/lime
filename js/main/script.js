356f47a607e7891d02442ab82d5ff380c952557e
PAVEL <mindalev77@mail.ru>
2024-01-24 10:20:08 +0300

    12

--- js/main/script.js
+++ js/main/script.js
@@ -52,4 +52,16 @@ document.addEventListener('DOMContentLoaded', function () {
         },
     });
 });
-
+document.addEventListener('DOMContentLoaded', function () {
+    var swiper = new Swiper(".build_swiper--mobile", {
+        spaceBetween: 10,
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+    });
+});
